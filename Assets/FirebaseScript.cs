905c72adcdf944e8c0b6bd51d2272adf99f85aeb
danyal97 <danyalaftab97@gmail.com>
2020-08-10 20:08:58 +0500

    Done

--- Assets/FirebaseScript.cs
+++ Assets/FirebaseScript.cs
@@ -1,24 +1,32 @@
 ﻿using Firebase;
+using Firebase.Auth;
 using Firebase.Database;
 using Firebase.Unity.Editor;
 
 
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Connect;
 using UnityEngine;
 
 public class FirebaseScript : MonoBehaviour
 {
 
     DatabaseReference reference;
-
+    Firebase.Auth.FirebaseAuth auth;
     // Start is called before the first frame update
     void Start()
     {
+       
+
+        
         // #1
-        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task =>
+        {
             var dependencyStatus = task.Result;
             if (dependencyStatus == Firebase.DependencyStatus.Available)
+
             {
                 // Create and hold a reference to your FirebaseApp,
                 // where app is a Firebase.FirebaseApp property of your application class.
@@ -36,11 +44,12 @@ public class FirebaseScript : MonoBehaviour
 
         // #2
         // Set up the Editor before calling into the realtime database.
-        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("");
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://durakcard-2a29c.firebaseio.com/");
 
         // #3
         // Get the root reference location of the database.
         reference = FirebaseDatabase.DefaultInstance.RootReference;
+        
 
         //_________________________
 
@@ -48,10 +57,11 @@ public class FirebaseScript : MonoBehaviour
         Debug.Log(DebugMsg); // print message in console
 
     }
-    
+
     public string saveDataToFirebase(int id, string name, string city) // نفرض اننا حابين نعمل قاعدة بيانات للموظفين كل موظف عنده بيانات مثل ID, Name, City
     {
-        reference.Child(id.ToString()).Child("Name").SetValueAsync(name); 
+
+        reference.Child(id.ToString()).Child("Name").SetValueAsync(name);
         reference.Child(id.ToString()).Child("City").SetValueAsync(city);
         return "Save data to firebase Done.";
     }
