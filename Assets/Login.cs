df2bd25a88be9d37b58d57232ea0392a40fca87c
moizdilnawaz <31615938+moizdilnawaz@users.noreply.github.com>
2020-08-11 18:13:58 +0500

    done

---
+++ Assets/Login.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Login : MonoBehaviour
+{
+    public Text email;
+    public Text password;
+    public InputField pass;
+    // Start is called before the first frame update
+    public void onSubmit() {
+
+        Debug.Log("Email" + email.text +" Password"+pass.text);
+    }
+}
